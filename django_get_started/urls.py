f657ad552e75c2a781a944601f34c158836bcf12
Daniel Campos <danielcampos1012@gmail.com>
2018-10-30 21:48:09 -0300

    Update urls.py

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -35,7 +35,7 @@ urlpatterns = patterns('',
             'next_page': '/',
         },
         name='logout'),
-    url(r'^cadastro_vestibulares', 'app.views.cadastro_vestibulares’, name='cadastro_vestibulares'),
+    url(r'^cadastro_vestibulares', 'app.views.cadastro_vestibulares’, name='cadastro_vestibulares')
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
