cff6f6b4999ff383ebe0ba42fff3f08ea394e530
Daniel Campos <danielcampos1012@gmail.com>
2018-10-30 21:54:11 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -66,10 +66,10 @@ def cadastro_vestibulares(request):
      assert isinstance(request, HttpRequest) 
      return render(
      request,
-    'app/cadastro_vestibulares.html’, 
+    'app/cadastro_vestibulares.html', 
      context_instance = RequestContext(request,
      {
-     'title':'Cadastro de vestibulares’,
+     'title':'Cadastro de vestibulares',
      'vestibulares': Vestibular.objects.all( ),
      'year':datetime.now().year, 
      })
