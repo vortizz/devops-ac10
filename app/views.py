c0f5571f5ad7d60ddc3097b7deec6cf33fb78ae8
vortizz <42192259+vortizz@users.noreply.github.com>
2018-10-30 22:14:23 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ Definition of views.
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
-from app.models import Curso, Vestibular
+from app.models import Curso, Vestibular, Candidato
 from datetime import datetime
 
 def home(request):
